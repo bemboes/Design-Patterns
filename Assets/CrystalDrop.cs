64f8d7e2c932829bd2b07966967b7a8f9a85d811
Michael-PC\Michael <michael112911@gmail.com>
2017-04-24 15:30:49 +0200

    commiting whole project final

---
+++ Assets/CrystalDrop.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalDrop : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject crystal;
+    [SerializeField]
+    private GameObject[] dropjects;
+    private float DestroyTime = 5;
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(DestroyTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DestroyTime -= Time.deltaTime;
+        }
+
+	}
+}
